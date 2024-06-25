c691e4f44d59710822bdeb7692dda3bd9fcc8e9b
SilasMarvin <19626586+SilasMarvin@users.noreply.github.com>
2024-06-24 22:31:02 -0700

    Added vector store backend and ollama embedding option

---
+++ crates/lsp-ai/src/embedding_models/mod.rs
@@ -0,0 +1,28 @@
+use crate::config::ValidEmbeddingModel;
+
+mod ollama;
+
+#[derive(Clone, Copy)]
+pub enum EmbeddingPurpose {
+    Storage,
+    Retrieval,
+}
+
+#[async_trait::async_trait]
+pub trait EmbeddingModel {
+    async fn embed(
+        &self,
+        batch: &[&str],
+        purpose: EmbeddingPurpose,
+    ) -> anyhow::Result<Vec<Vec<f32>>>;
+}
+
+impl TryFrom<ValidEmbeddingModel> for Box<dyn EmbeddingModel> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: ValidEmbeddingModel) -> Result<Self, Self::Error> {
+        match value {
+            ValidEmbeddingModel::Ollama(config) => Ok(Box::new(ollama::Ollama::new(config))),
+        }
+    }
+}
---
+++ crates/lsp-ai/src/embedding_models/ollama.rs
@@ -0,0 +1,102 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+use serde_json::{json, Value};
+
+use crate::config;
+
+use super::{EmbeddingModel, EmbeddingPurpose};
+
+#[derive(Deserialize)]
+pub struct EmbedResponse {
+    embedding: Option<Vec<f32>>,
+    error: Option<Value>,
+    #[serde(default)]
+    #[serde(flatten)]
+    other: HashMap<String, Value>,
+}
+
+pub struct Ollama {
+    config: config::OllamaEmbeddingModel,
+}
+
+impl Ollama {
+    pub fn new(config: config::OllamaEmbeddingModel) -> Self {
+        Self { config }
+    }
+}
+
+#[async_trait::async_trait]
+impl EmbeddingModel for Ollama {
+    async fn embed(
+        &self,
+        batch: &[&str],
+        purpose: EmbeddingPurpose,
+    ) -> anyhow::Result<Vec<Vec<f32>>> {
+        let mut results = vec![];
+        let prefix = match purpose {
+            EmbeddingPurpose::Storage => &self.config.prefix.storage,
+            EmbeddingPurpose::Retrieval => &self.config.prefix.retrieval,
+        };
+        let client = reqwest::Client::new();
+        for item in batch {
+            let prompt = format!("{prefix}{item}");
+            let res: EmbedResponse = client
+                .post(
+                    self.config
+                        .endpoint
+                        .as_deref()
+                        .unwrap_or("http://localhost:11434/api/embeddings"),
+                )
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .json(&json!({
+                    "model": self.config.model,
+                    "prompt": prompt
+                }))
+                .send()
+                .await?
+                .json()
+                .await?;
+            if let Some(error) = res.error {
+                anyhow::bail!("{:?}", error.to_string())
+            } else if let Some(embedding) = res.embedding {
+                results.push(embedding);
+            } else {
+                anyhow::bail!(
+                    "Unknown error while making request to Ollama: {:?}",
+                    res.other
+                )
+            }
+        }
+        Ok(results)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn ollama_embeding() -> anyhow::Result<()> {
+        let configuration: config::OllamaEmbeddingModel = serde_json::from_value(json!({
+            "model": "nomic-embed-text",
+            "prefix": {
+                "retrieval": "search_query",
+                "storage": "search_document"
+            }
+        }))?;
+
+        let ollama = Ollama::new(configuration);
+        let results = ollama
+            .embed(
+                &["Hello world!", "How are you?"],
+                EmbeddingPurpose::Retrieval,
+            )
+            .await?;
+        assert_eq!(results.len(), 2);
+        assert_eq!(results[0].len(), 768);
+
+        Ok(())
+    }
+}
